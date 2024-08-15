41fe1ec6d164f3112ea9f7f6472a193206a8f457
samajestecodeur <issa97403@gmail.com>
2024-08-15 10:59:12 +0200

    addaé

--- configs/config.js
+++ configs/config.js
@@ -4,7 +4,6 @@
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
-    endpointId: process.env.ENDPOINT_ID,
     dialect: "postgres",
     url: process.env.URL,
     jwt: {
